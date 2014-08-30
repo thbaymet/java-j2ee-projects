ee08d763185fa889d4799f6dc2c17c20f8846279
bayramovmatin@gmail.com <bayramovmatin@gmail.com@97cf391b-038b-efdd-6190-ecd8f3925cd7>
2014-08-30 18:01:19 +0000

    login logout

--- yunusproject/src/main/java/com/yunus/org/services/impl/UserServiceImpl.java
+++ yunusproject/src/main/java/com/yunus/org/services/impl/UserServiceImpl.java
@@ -3,6 +3,16 @@
  */
 package com.yunus.org.services.impl;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
 import com.yunus.org.dao.UserDao;
 import com.yunus.org.domain.UserEntity;
 import com.yunus.org.services.UserService;
@@ -14,7 +24,7 @@ import com.yunus.org.services.UserService;
  * @author BAYRAMOV Matin
  *
  */
-public class UserServiceImpl implements UserService {
+public class UserServiceImpl implements UserService, UserDetailsService {
 
 	private UserDao userDao;
 	
@@ -38,6 +48,25 @@ public class UserServiceImpl implements UserService {
 	 */
 	public void setUserDao(UserDao userDao) {
 		this.userDao = userDao;
+	}
+
+	/**
+	 * Construct UserDetails instance required by spring security
+	 */
+	public UserDetails loadUserByUsername(String userName)
+			throws UsernameNotFoundException {
+		UserEntity user = userDao.loadUserByUserName(userName);
+		
+		if (user == null) {
+			throw new UsernameNotFoundException(String.format("No such user with name provided '%s'", userName));
+		}
+		
+		Collection<GrantedAuthority> authorities = new ArrayList<GrantedAuthority>();
+		authorities.add(new SimpleGrantedAuthority("ROLE_USER"));
+		
+		User userDetails = new User(user.getUserName(), user.getPassword(), authorities);
+		
+		return userDetails;
 	}
 
 }
