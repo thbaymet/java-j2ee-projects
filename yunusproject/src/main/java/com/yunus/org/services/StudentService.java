461d8416056b4ed9d7217d59155b3c73d477521b
bayramovmatin@gmail.com <bayramovmatin@gmail.com@97cf391b-038b-efdd-6190-ecd8f3925cd7>
2014-12-11 16:09:47 +0000

    Student dao, jpaDao, serviceInterface and its implementation classes added

---
+++ yunusproject/src/main/java/com/yunus/org/services/StudentService.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package com.yunus.org.services;
+
+import java.util.List;
+
+import com.yunus.org.domain.StudentEntity;
+
+/**
+ * @author BAYRAMOV Matin
+ *
+ */
+public interface StudentService {
+	
+	boolean createStudent(StudentEntity entity);
+	
+	boolean updateStudent(StudentEntity entity);
+	
+	boolean deleteStudent(StudentEntity entity);
+	
+	List<StudentEntity> getAllStudent();
+
+}
