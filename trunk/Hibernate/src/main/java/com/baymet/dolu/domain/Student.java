7bc5af78247ba598a36acda4cd1d8b615a36990d
bayramovmatin@gmail.com <bayramovmatin@gmail.com@97cf391b-038b-efdd-6190-ecd8f3925cd7>
2014-12-24 15:09:32 +0000

    Hibernate project first commit

---
+++ trunk/Hibernate/src/main/java/com/baymet/dolu/domain/Student.java
@@ -0,0 +1,140 @@
+/*
+ * Copyright (c) 2014 BAYRAMOV Matin
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.baymet.dolu.domain;
+
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author BAYRAMOV Matin
+ *
+ */
+public class Student {
+	
+	private Long id;
+	
+	private String firstName;
+	
+	private String lastName;
+	
+	private Date birthday;
+	
+	private Address address;
+	
+	private Set<Course> courses = new HashSet<Course>(0);
+	
+	public Student() {
+		super();
+	}
+	
+	public Student(String firstName, String lastName) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+
+	/**
+	 * @return the id
+	 */
+	public Long getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the firstName
+	 */
+	public String getFirstName() {
+		return firstName;
+	}
+
+	/**
+	 * @param firstName the firstName to set
+	 */
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	/**
+	 * @return the lastName
+	 */
+	public String getLastName() {
+		return lastName;
+	}
+
+	/**
+	 * @param lastName the lastName to set
+	 */
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	/**
+	 * @return the birthday
+	 */
+	public Date getBirthday() {
+		return birthday;
+	}
+
+	/**
+	 * @param birthday the birthday to set
+	 */
+	public void setBirthday(Date birthday) {
+		this.birthday = birthday;
+	}
+
+	/**
+	 * @return the address
+	 */
+	public Address getAddress() {
+		return address;
+	}
+
+	/**
+	 * @param address the address to set
+	 */
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+	
+	/**
+	 * @return the courses
+	 */
+	public Set<Course> getCourses() {
+		return courses;
+	}
+
+	/**
+	 * @param courses the courses to set
+	 */
+	public void setCourses(Set<Course> courses) {
+		this.courses = courses;
+	}
+
+	@Override
+    public String toString() {
+        return "Student [id=" + id + ", firstName=" + firstName + ", address="
+                + address + "]";
+    }
+
+}
